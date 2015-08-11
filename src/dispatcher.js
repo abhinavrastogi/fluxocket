f7a26d9817881aa2d7e979f35c63839e7eb4b6d3
Abhinav Rastogi <r.abhinav@gmail.com>
2015-08-11 21:53:47 +0530

    Adds dispatcher

---
+++ src/dispatcher.js
@@ -0,0 +1,23 @@
+import {Dispatcher as _Dispatcher} from 'flux';
+
+var Dispatcher = new _Dispatcher();
+
+var _dispatch = Dispatcher.dispatch.bind(Dispatcher);
+
+Dispatcher.setSocket = function(_socket) {
+	Dispatcher._socket = _socket;
+
+	_socket.on('flux_action', payload => {
+		Dispatcher.dispatch(payload, false);
+});
+};
+
+Dispatcher.dispatch = function(payload, broadcast = true) {
+	if(broadcast) {
+		Dispatcher._socket.emit('flux_action', payload);
+	}
+
+	return _dispatch(payload);
+};
+
+export default Dispatcher;
